49aa1b240a3a6afad81ca88912813c873fba2f5d
nakibulhosen <nakibulhosen7@gmail.com>
2022-10-04 11:11:00 +0600

    product home page

---
+++ pages/product/index.js
@@ -0,0 +1,13 @@
+const Products = () => {
+    return (
+        <div>
+            <ol>
+                <li>Product - 1</li>
+                <li>Product - 2</li>
+                <li>Product - 3</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Products
