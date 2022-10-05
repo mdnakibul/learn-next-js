4baff2a9f190a126028ac0fdc71bea9f8a7940a3
nakibulhosen <nakibulhosen7@gmail.com>
2022-10-05 10:13:34 +0600

    use replace prop in link component

--- pages/product/index.js
+++ pages/product/index.js
@@ -6,7 +6,7 @@ const Products = ({ productId = 100 }) => {
             <ol>
                 <li><Link href="/product/1"><a>Product - 1</a></Link></li>
                 <li><Link href="/product/2"><a>Product - 2</a></Link></li>
-                <li><Link href="/product/3"><a>Product - 3</a></Link></li>
+                <li><Link href="/product/3" replace><a>Product - 3</a></Link></li>
                 <li><Link href={`/product/${productId}`}><a>Product - {productId}</a></Link></li>
             </ol>
 
