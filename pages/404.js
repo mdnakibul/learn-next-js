570c21757584e063f48d3876648148d5cf773b48
Md Nakibul Hosen Nahid Nahid <nakibulh.aobs@gmail.com>
2023-04-22 12:48:57 +0600

    change message of 404 error

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PageNotFound = () => {
     return (
         <div>
-            <h2>Page Not Found</h2>
+            <h2>Page Not Found. A Custom 404 Page</h2>
         </div>
     );
 };
